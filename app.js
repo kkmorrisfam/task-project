e5a7489784f4b73cf7d0dd1f6abd60869d1ce952
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-19 21:36:34 -0800

    add routes and controller to test

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const express = require("express")
-const app = express
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const dotenv = require('dotenv');
+dotenv.config();
 
-const PORT = 5150
+//middleware
+app.use(express.json());
 
-app.listen(PORT, console.log(`server is listening on port ${PORT}...`))
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')        - get all the tasks
+// app.post('/api/v1/tasks')       - create a new task
+// app.get('/api/v1/tasks/:id')    - get single task
+// app.patch('/api/v1/tasks/:id')  - update task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.listen(port, console.log(`server is listening on ${host}:${port}...`));
