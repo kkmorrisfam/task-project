b5212efe973febbdd5303f626c7acc59f73e6fca
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-24 19:10:29 -0800

    update controllers, get database working

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,34 @@
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
+const Task = require('../models/task')
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({ })
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+//need to put this in a try/catch block
+const createTask = async (req, res) => { 
+    try {
+        // const task = await Task.create({name:'first task'})
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    
 }
 
 const getTask = (req, res) => {
-    res.json({id:req.params.id})
+    try {
+        res.json({id:req.params.id})
+    } catch (error) {
+
+    }
+    
 }
 
 const updateTask = (req, res) => {
---
+++ models/task.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+//create the structure for the documents in 
+//the collection. Only these properties will be
+//excepted, additional values will be ignored
+const taskSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true, 'must provide name'],  //can just use "required:true", but adding array, can include error message
+        trim:true,
+        maxlength:[20,'name cannot be more than 20 characters']
+    }, 
+    completed: {
+        type: Boolean,
+        default: false  //start by creating new tasks, and not completed
+    }
+
+})
+
+module.exports = mongoose.model('Task', taskSchema)
