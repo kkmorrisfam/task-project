9e1ad60324dddee22709be0d616c42ae61c252d5
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-02-25 18:08:49 -0800

    add error custom class

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,8 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
 //   return res.status(500).json({ msg: err });
     console.log(err)
-  return res.status(500).json({msg: 'Something went wrong, try again later.'})  // can also hard code a message, not as large
+  return res.status(err.status).json({msg: err.message})  // can also hard code a message, not as large
+                                                        // err.status and err.message uses object values passed in
 };
 
 module.exports = errorHandlerMiddleware
